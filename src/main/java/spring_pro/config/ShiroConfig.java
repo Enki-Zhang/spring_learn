02334b9df6aa2b0f80df27d9a8103d557e30aaf5
Enki <17855378518@163.com>
2024-08-12 17:21:58 +0800

    shiro登录框架

--- src/main/java/spring_pro/config/ShiroConfig.java
+++ src/main/java/spring_pro/config/ShiroConfig.java
@@ -1,5 +1,15 @@
 package spring_pro.config;
 
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.shiro.realm.Realm;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @ClassName ShiroConfig
  * @Description Shiro登录校验配置
@@ -7,5 +17,38 @@ package spring_pro.config;
  * @Date 2024/8/12 17:11
  * @Verison 1.0
  **/
+@Configuration
 public class ShiroConfig {
+    @Bean
+    public Realm myBealm() {
+//        return new MyRealm();
+    }
+
+    @Bean
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(myRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        // 配置 URL 过滤规则
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        filterChainDefinitionMap.put("/login", "anon"); // 不需要认证
+        filterChainDefinitionMap.put("/admin/**", "authc"); // 需要认证
+        filterChainDefinitionMap.put("/**", "authc"); // 其他所有 URL 需要认证
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
 }
---
+++ src/main/java/spring_pro/shiroLogin/MyRealm.java
@@ -0,0 +1,11 @@
+package spring_pro.shiroLogin;
+
+/**
+ * @ClassName MyRealm
+ * @Description TODO
+ * @Author Enki
+ * @Date 2024/8/12 17:21
+ * @Verison 1.0
+ **/
+public class MyRealm {
+}
